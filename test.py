4d97f43f674cf41a7f3cf13c4499b25c78eadb0d
aimelquijada <quijadaaimel@gmail.com>
2024-07-14 12:21:19 -0400

    Se agrega el proyecto de Euler

---
+++ test.py
@@ -0,0 +1,12 @@
+#Aimel Quijada. C.I: 30.729.553
+import unittest
+from euler import *
+
+class MyTest(unittest.TestCase):
+    
+    def euler(self):  
+    	pass
+        
+
+if __name__ == '__main__':
+    unittest.main()
